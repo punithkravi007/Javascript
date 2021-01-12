612c829971becf7da916105fc9576b16dab543de
Punith KR <68544398+punithkravi007@users.noreply.github.com>
2021-01-12 15:31:14 +0530

    Created Spread and Rest Operator
    Spread : It spread the array of elements or objects into function arguments
    
    Rest : it compress the multiple arguments into array of elements later which is iterrated to perform action

---
+++ 01-Intermediate/Spread-Rest-Operators.js
@@ -0,0 +1,38 @@
+//---------------------------------------------------------------------------------------------------------------
+//TODO: Spread Operartor converts the Array or Objects into induvidual args to function
+//---------------------------------------------------------------------------------------------------------------
+const sumOfTwoNumbers = (num1, num2) => {
+    return num1 + num2;
+}
+
+console.log(sumOfTwoNumbers(1, 2));
+
+var arr = [2, 4, 6, 8]
+
+// TODO: Here arr is passed as first argument to the function and second arg is undefined.
+console.log(sumOfTwoNumbers(arr));
+
+//TODO: ...arr is now spreaded and first two elements of array are passed as arguments and rest are ignored
+console.log(sumOfTwoNumbers(...arr));
+//---------------------------------------------------------------------------------------------------------------
+
+//---------------------------------------------------------------------------------------------------------------
+//TODO: Rest Operator takes n argument and converts it into array
+//---------------------------------------------------------------------------------------------------------------
+
+const sumN = (...args) => {
+    let sum = 0;
+    args.forEach((arg) => sum += arg)
+    return sum;
+}
+
+console.log(sumN(1,2,3,4,5,6));
+
+const multiplyAndSum = (num1, num2, ...args) => {
+    let multiple = num1 * num2;
+    let sum = 0;
+    args.forEach(e => sum += parseInt(e));
+    return [multiple, sum];
+}
+
+console.log(multiplyAndSum(2, 3, 4, 5, 6, 7, 8));
